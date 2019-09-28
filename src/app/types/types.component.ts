4ed7a6b876973729067241a5aaeaea802a908d55
PiotOw <powczarczyk@vp.pl>
2019-09-28 04:45:48 +0200

    type emblems list rearrange color profile and navbar development

--- src/app/types/types.component.ts
+++ src/app/types/types.component.ts
@@ -11,18 +11,45 @@ export class TypesComponent implements OnInit {
 	types = TYPES;
 
 	breakpoint: number;
-	breakpoints: string;
+	class: string;
+	// breakpoints: string;
 
 	constructor() {
 	}
 
+	// ngOnInit() {
+	// 	this.breakpoint = Math.floor(window.innerWidth / 220);
+	// 	if(this.breakpoint > 5) {
+	// 		this.breakpoint = 5;
+	// 	}
+	// 	this.breakpoints = this.breakpoint * 220 + "px";
+	// 	console.log(this.breakpoint);
+	// }
+	//
+	// onClickTest(index: number) {
+	// 	console.log(this.types[index]);
+	// }
+	//
+	// onResize(event) {
+	// 	this.breakpoint = Math.floor(event.target.innerWidth / 220);
+	// 	if(this.breakpoint > 5) {
+	// 		this.breakpoint = 5;
+	// 	}
+	// 	this.breakpoints = this.breakpoint * 220 + "px";
+	// 	console.log(this.breakpoint);
+	// 	console.log(this.breakpoints);
+	// }
+	//
 	ngOnInit() {
-		this.breakpoint = Math.floor(window.innerWidth / 220);
+		this.breakpoint = Math.floor(window.innerWidth / 210);
 		if(this.breakpoint > 5) {
 			this.breakpoint = 5;
 		}
-		this.breakpoints = this.breakpoint * 220 + "px";
-		console.log(this.breakpoint);
+		else if(this.breakpoint == 0){
+			this.breakpoint = 1;
+		}
+		this.class = "type-emblem-container-" + this.breakpoint;
+		console.log(this.class);
 	}
 
 	onClickTest(index: number) {
@@ -30,13 +57,16 @@ export class TypesComponent implements OnInit {
 	}
 
 	onResize(event) {
-		this.breakpoint = Math.floor(event.target.innerWidth / 220);
+		this.breakpoint = Math.floor(window.innerWidth / 210);
 		if(this.breakpoint > 5) {
 			this.breakpoint = 5;
 		}
-		this.breakpoints = this.breakpoint * 220 + "px";
-		console.log(this.breakpoint);
-		console.log(this.breakpoints);
+		else if(this.breakpoint == 0){
+			this.breakpoint = 1;
+		}
+		this.class = "type-emblem-container-" + this.breakpoint;
+		console.log(this.class);
 	}
+
 
 }
