6bcdbbaaa70d061cc0003a7b4705090358138864
piotrowczarczyk <powczarczyk@vp.pl>
2023-03-24 15:31:20 +0100

    update

--- src/app/shared/pikachu-service/pikachu.service.ts
+++ src/app/shared/pikachu-service/pikachu.service.ts
@@ -14,4 +14,15 @@ export class PikachuService {
 
 	constructor() {
 	}
+
+
+  onFileSelected(event: Event) {
+    const inputNode1: any = document.querySelector('#file'); // to jest ten input do którego dodano plik
+    const inputNode2: any = event.target; // to też jest ten input do którego dodano plik
+
+    const file1 = inputNode1.files[0];
+    const file2 = inputNode2.files[0];
+  }
+
+
 }
